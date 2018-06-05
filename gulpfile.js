550765e21561fdbb9b9161ded03c22fb6e1ed164
lokesh1990 <lokesh1990@gmail.com>
2018-06-04 23:56:41 -0400

    split css, customize; partial work

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,29 @@
 'use strict'
 
 const gulp = require('gulp')
-const requireDir = require('require-dir')
+const concat = require('gulp-concat');
+const minifyCSS = require('gulp-minify-css');
 
-requireDir('./gulp/', {recurse: true})
-
-gulp.task('default', ['clean'], function () {
-  gulp.start('build')
+gulp.task('buildcss', function() {
+  var files = [
+    "css/resume.css",
+    "css/sidebar.css",
+    "css/photo.css",
+    "css/education.css",
+    "css/content.css",
+    "css/name.css",
+    "css/summary.css",
+    "css/work.css",
+    "css/volunteer.css",
+    "css/publication.css",
+    "css/awards.css",
+    "css/languages.css",
+    "css/interests.css",
+    "css/skills.css",
+    "css/contact.css",
+    "css/media.css"];
+  return gulp.src(files)
+    .pipe(minifyCSS())
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./'));
 })
--- index.js
+++ index.js
@@ -1,93 +1,90 @@
-var fs = require('fs')
-var path = require('path')
-var Handlebars = require('handlebars')
-var moment = require('moment')
-var pluralize = require('pluralize')
+var fs = require("fs");
+var Handlebars = require("handlebars");
+var moment = require("moment");
+var pluralize = require('pluralize');
+var path = require('path');
 
-function render (resume) {
-  var css = fs.readFileSync(path.join(__dirname, '/style.css'), 'utf-8')
-  var tpl = fs.readFileSync(path.join(__dirname, '/resume.hbs'), 'utf-8')
-  var partialsDir = path.join(__dirname, 'partials')
-  var filenames = fs.readdirSync(partialsDir)
+function render(resume) {
+	var css = fs.readFileSync(path.join(__dirname, "/style.css"), "utf-8");
+	var tpl = fs.readFileSync(path.join(__dirname, "/resume.hbs"), "utf-8");
+	var partialsDir = path.join(__dirname, "partials");
+	var filenames = fs.readdirSync(partialsDir);
 
-  Handlebars.registerHelper({
-    formatDate: function (date) {
-      if (typeof date === 'undefined') {
-        return 'now'
-      }
-      return moment(date).format('MMM YYYY')
-    },
-    formatDateYear: function (date) {
-      if (typeof date === 'undefined') {
-        return 'now'
-      }
-      return moment(date).format('YYYY')
-    },
-    networkIcon: function (network) {
-      if (network === 'StackOverflow') {
-        return 'stack-overflow'
-      } else {
-        return network.toLowerCase()
-      }
-    },
-    wordWrap: function (str) {
-      str = str.replace(/\//g, "/ ");
-      return str.replace("/ / ", "//");
-    },
-    dateDiff: function (startDate, endDate) {
-      let text = ''
-      startDate = moment(startDate)
-      if (endDate === null || endDate === '' || endDate === undefined) {
-        endDate = moment()
-      } else {
-        endDate = moment(endDate)
-      }
-      let years = endDate.diff(startDate, 'years')
-      startDate.add(years, 'years')
-      let months = endDate.diff(startDate, 'months')
-
-      if (years > 0) {
-        text += `${years} ${pluralize('years', years)}`
-      }
-      if (months > 0) {
-        if (years > 0) {
-          text += ' '
-        }
-        text += `${months} ${pluralize('months', months)}`
-      }
-
-      return text
+	filenames.forEach(function(filename) {
+    var matches = /^([^.]+).hbs$/.exec(filename);
+    if(!matches) {
+      return;
     }
-  })
+    var name = matches[1];
+    var filepath = path.join(partialsDir, filename);
+    var template = fs.readFileSync(filepath, "utf8");
 
-  filenames.forEach(function (filename) {
-    var matches = /^([^.]+).hbs$/.exec(filename)
-    if (!matches) {
-      return
-    }
-    var name = matches[1]
-    var filepath = path.join(partialsDir, filename)
-    var template = fs.readFileSync(filepath, 'utf8')
+    Handlebars.registerPartial(name, template);
+	});
 
-    Handlebars.registerPartial(name, template)
-  })
-  return Handlebars.compile(tpl)({
-    css: css,
-    resume: resume
-  })
+	return Handlebars.compile(tpl)({
+		css: css,
+		resume: resume
+	});
 }
 
-function exportPdf (resumeFile, pageFormat) {
-  let resume = require(path.join(__dirname, resumeFile))
-  const pdf = require('html-pdf')
-  const template = render(resume, pageFormat)
+function exportPdf(resumeFile, pageFormat) {
+  var resume = require(__dirname + resumeFile);
+  const pdf = require('html-pdf');
+  const template = render(resume, pageFormat);
 
   pdf.create(template, {format: pageFormat}).toFile('./resume.pdf', function (err, res) {
-    if (err) return console.log(err)
-  })
+    if (err) {
+			return console.log(err);
+		}
+  });
 }
 
 module.exports = {
-  render: render,
+	render: render,
   exportPdf: exportPdf
-}
+};
+
+/* HANDLEBARS HELPERS */
+Handlebars.registerHelper('toLowerCase', function(str) {
+  return str.toLowerCase();
+});
+
+Handlebars.registerHelper('spaceToDash', function(str) {
+  return str.replace(/\s/g, '-').toLowerCase();
+});
+
+Handlebars.registerHelper('formatDate', function(date) {
+	if(typeof date === 'undefined') {
+		return 'present';
+	}
+	return date;
+});
+
+Handlebars.registerHelper('formatDateYear', function(date) {
+	if(typeof date === 'undefined') {
+		return 'present';
+	}
+	return date;
+});
+
+Handlebars.registerHelper('networkIcon', function(network) {
+	if(network === 'StackOverflow') {
+        return 'stack-overflow';
+  }
+	else {
+        return network.toLowerCase();
+	}
+});
+
+Handlebars.registerHelper('wordWrap', function(str) {
+	str = str.replace(/\//g, "/");
+	return str.replace("/ / ", "//");
+});
+
+Handlebars.registerHelper('equals', function(src, target, options) {
+	if(typeof src !== undefined && src.length > 0 && src === target) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
